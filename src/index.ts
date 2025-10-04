db09c1a34543ff031e49012307881a25d251b588
krupal2805 <krupalsiddhapura@gmail.com>
2025-10-05 00:09:58 +0530

    prisma setup

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@as-integrations/express5';
 import cors from 'cors';
+import {prismaClient} from './lib/db.js'
 
 async function init() {
     const app = express()
@@ -17,11 +18,30 @@ async function init() {
                 hello : String
                 say(name : String) : String
             }
+            
+            type Mutation {
+                createUser(firstName: String!,lastName: String!,email: String!,password: String!) : Boolean
+            }
         `,
         resolvers: {
             Query : {
                 hello : () => 'Hey there i am graphql server',
                 say : (_,{name}:{name : String}) => `Hey ${name} how are you ?`  
+            },
+            Mutation : {
+                createUser : async (_,{firstName,lastName,email,password}:{firstName:string;lastName:string;email:string;password:string}) => {
+                    await prismaClient.user.create({
+                        data : {
+                            firstName,
+                            lastName,
+                            email,
+                            password,
+                            salt : 'random_salt'
+                        }
+                    })
+                    return true;
+                }
+
             }
         }
     })
